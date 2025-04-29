cf50d04963a509b52bc514e118d14992f150731b
veryramas69 <veryramas@gmail.com>
2025-04-29 19:53:03 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$config = array(
+  	'url'		=> 'https://uhe.edu.pk/run/',
+	'parameter' => 'proyek',
+	'template'  => 'temp12.html',
+	'keywords'  => 'cuan.txt',
+	'sitemap'	=> 'sitemap.xml'
+);
+?>
